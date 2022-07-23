bbaf83d7953c282cde0a43b91af1273e7603e4ec
tariqalsadi <tariqalsadi@gmail.com>
2022-07-23 16:07:07 +0300

    init
    git commit -m init

---
+++ trainer_app/trainer_app/doctype/trainer/test_trainer.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, pu and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestTrainer(unittest.TestCase):
+	pass
---
+++ trainer_app/trainer_app/doctype/trainer_course/trainer_course.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, pu and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Trainer Course', {
+	// refresh: function(frm) {
+
+	// }
+});
